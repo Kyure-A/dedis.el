1021b4a410e85d0b8fdaf845e6006b3716bd41db
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-06-17 20:39:30 +0900

    [Initialized] commit

---
+++ detecting-distro.el
@@ -0,0 +1,38 @@
+;;; detecting-destro.el --- detecting linux distribution  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Kyure_A
+
+;; Author: Kyure_A <k@kyre.moe>
+;; Keywords: tools
+
+;; Version: 0.0.0
+;; Package-Requires: ((emacs "24.1"))
+;; URL: https://github.com/Kyure-A/detecting-distro.el
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; detecting linux distribution
+
+;;; Code:
+
+(defgroup detecting-destro ()
+  "detecting linux distribution"
+  :group 'tools
+  :prefix "detecting-destro-"
+  :link '(url-link "https://github.com/Kyure-A/detecting-distro.el"))
+
+(provide 'detecting-destro)
+;;; detecting-destro.el ends here
